2f45acc54aa1d75a798ac35af721091b3fef7e34
usuarioutnfe <cursofeutn@gmail.com>
2021-05-14 08:09:31 -0300

    Agregado del .env

--- bin/connection.ts
+++ bin/connection.ts
@@ -1,11 +1,11 @@
 import mysql from 'mysql';
-
+require('dotenv').config();
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "bdappservicios",
-    port: 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
 })
 
 
